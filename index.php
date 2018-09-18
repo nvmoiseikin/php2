d74eb611a134595fd8fd1ee907c519557d6e3a5d
nvmoiseikin <nvmoiseikin@gmail.com>
2018-09-18 18:58:07 +0500

    create index.php
    start git

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Moiseikin</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+</body>
+</html>
