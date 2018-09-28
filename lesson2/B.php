2f9f414b7853d6ba8808c3ca9def59732e7b9f41
nvmoiseikin <nvmoiseikin@gmail.com>
2018-09-28 21:17:24 +0500

    Подклассы товаров(3 штуки)

---
+++ lesson2/B.php
@@ -0,0 +1,29 @@
+<?php
+class B extends MainClass {
+	
+	public function __construct($myQuantity){
+		$this->quantity = $myQuantity;
+		$this->calcProductProfit();
+		/*** tests ***/
+		$this->getProductProfit();
+		$this->getFullClassProfit();
+		$this->getFullProfit();
+	}
+	
+	/*** calculating ***/
+	private function calcProductProfit(){
+		$this->productProfit = $this->quantity*MainClass::mainPrice;
+		$this->addClassProfit($this->productProfit);
+		MainClass::$fullProfit += $this->productProfit;
+	}
+	
+	/*** getters ***/
+	public function getFullClassProfit(){
+		echo "Общий доход ШТУЧНЫХ товаров составил ".$this->addClassProfit(0)." денежных едениц<br>";
+	}
+	
+	public function getProductProfit(){
+		echo "Общий доход ЭТОГО товара составил ".$this->productProfit." денежных едениц<br>";
+	}
+	
+}
